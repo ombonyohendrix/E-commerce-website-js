2fd775575ee3735f23c8f0c2b760bff86c1baa2b
ombonyohendrix <ombonyohendrix@gmail.com>
2024-11-12 10:32:29 +0300

    delete products from HTML when clicking delete

--- scripts/checkout.js
+++ scripts/checkout.js
@@ -15,7 +15,8 @@ cart.forEach((cartItem) => {
 	//console.log(matchingProduct);
 
 	cartSummaryHTML += `
-<div class="cart-item-container">
+<div class="cart-item-container 
+js-cart-item-container-${matchingProduct.id}">
        <div class="delivery-date">
               Delivery date: Tuesday, June 21
          </div>
@@ -102,6 +103,11 @@ document.querySelectorAll(".js-delete-link").forEach((link) => {
 		//console.log(productId);
 		removeFromCart(productId);
 		//console.log(cart);
+		const container = document.querySelector(
+			`.js-cart-item-container-${productId}`
+		);
+		//console.log(container);
+		container.remove();
 	});
 });
 //console.log(cartSummaryHTML);
